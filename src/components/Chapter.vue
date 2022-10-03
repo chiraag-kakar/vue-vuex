45bc3dcdfd7fb1db4f05bc8ac455e7cddfd530bb
Chiraag Kakar <chiraag.kakar@codapayments.com>
2022-10-03 19:51:53 +0530

    modified:   src/components/Chapter.vue

--- src/components/Chapter.vue
+++ src/components/Chapter.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex-container">
+  <div class="flex-container" v-if="books">
     <div v-for="(todo2, index2) in books[data].chapter_ids" :key="index2">
       <button class="justify-content mr-2" @click="method(todo2)">{{todo2}}</button>
     </div>
   </div>
   {{getChapterDetails}}
-</template>   
+</template>
 <script>
 import { mapState } from 'vuex';
 import { defineComponent } from 'vue';
@@ -19,9 +19,14 @@ export default defineComponent({
       e: {},
     }
   },
+  async beforeMount(){
+    console.log('before')
+    const f = this.books[this.data].chapter_ids[0]
+    await this.$store.dispatch("mangaModule/getChapterDetails", f)
+  },
   methods: {
-    method(cid) {
-      this.$store.dispatch("mangaModule/getChapterDetails", cid)
+    async method(cid) {
+      await this.$store.dispatch("mangaModule/getChapterDetails", cid)
     }
 
   },
@@ -36,7 +41,7 @@ export default defineComponent({
       getChapterDetails: function (state) {
           const c = state
           const d = JSON.parse(JSON.stringify(c))
-          const e = d.mangaModule.chapter_details.data
+          const e = d.mangaModule.chapter_details.data.pages
           if(e) return e
           else return []
         }
